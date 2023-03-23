713db96f35ce276efee62e9a5cd1b9713307dd9f
MelAkemi <meelakemi@gmail.com>
2023-03-23 17:11:00 -0300

    Acrescentandi nivel de nogos para o usuario

--- adivinha.c
+++ adivinha.c
@@ -5,9 +5,9 @@
 int main(){
 
     //imprime cabecalho do jogo
-    printf("******************************************");
-    printf("* Bem-vindo ao nosso jogo de adivinhação *");
-    printf("******************************************");
+    printf("******************************************\n");
+    printf("* Bem-vindo ao nosso jogo de adivinhacao *\n");
+    printf("******************************************\n");
 
     int segundos = time(0);
     srand(segundos);
@@ -21,7 +21,25 @@ int main(){
 
     int acertou = 0;
 
-    int numerodetentativas = 5;
+    int nivel;
+        printf("Qual nivel de dificuldade?\n");
+        printf("(1) Facil (2) Medio (3) Dificil\n\n");
+        printf("Escolha: ");
+        scanf("%d", &nivel);
+
+        int numerodetentativas  ;
+
+        if (nivel == 1){
+            numerodetentativas = 20;
+
+        }
+        else if (nivel == 2){
+            numerodetentativas = 15;
+        }
+        else {
+            numerodetentativas = 6;
+        }
+
 
     for(int i = 1; i <= numerodetentativas; i++) {
 
@@ -68,5 +86,5 @@ int main(){
         printf("Voce perdeu! Tente de novo!\n");
     }
 
-teste
+
 }
