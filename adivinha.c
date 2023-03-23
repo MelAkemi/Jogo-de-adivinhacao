87a212f19c7f30bb45ae981b1e9c4424bb5d141b
Melissa Akemi <92556113+MelAkemi@users.noreply.github.com>
2023-03-23 20:18:55 -0300

    final

--- adivinha.c
+++ adivinha.c
@@ -5,9 +5,16 @@
 int main(){
 
     //imprime cabecalho do jogo
-    printf("******************************************\n");
-    printf("* Bem-vindo ao nosso jogo de adivinhacao *\n");
-    printf("******************************************\n");
+    printf("\n\n");
+    printf("          P  /_\\  P                              \n");
+    printf("         /_\\_|_|_/_\\                            \n");
+    printf("     n_n | ||. .|| | n_n          Bem vindo ao    \n");
+    printf("     |_|_|nnnn nnnn|_|_|      Jogo de Adivinhação!\n");
+    printf("    |" "  |  |_|  |"  " |                         \n");
+    printf("    |_____| ' _ ' |_____|                         \n");
+    printf("          \\__|_|__/                              \n");
+    printf("\n\n");
+
 
     int segundos = time(0);
     srand(segundos);
@@ -82,13 +89,40 @@ int main(){
 
     printf("Fim de jogo!\n");
 
-    if(acertou) {
-        printf("Voce ganhou!\n");
-        printf("Voce acertou em %d tentativas!\n", tentativas);
-        printf("Total de pontos: %.1f\n", pontos);
-    } else {
-        printf("Voce perdeu! Tente de novo!\n");
-    }
+  if(acertou) {
+
+        printf("             OOOOOOOOOOO               \n");
+        printf("         OOOOOOOOOOOOOOOOOOO           \n");
+        printf("      OOOOOO  OOOOOOOOO  OOOOOO        \n");
+        printf("    OOOOOO      OOOOO      OOOOOO      \n");
+        printf("  OOOOOOOO  #   OOOOO  #   OOOOOOOO    \n");
+        printf(" OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  \n");
+        printf(" OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   \n");
+        printf("  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    \n");
+        printf("    OOOOO   OOOOOOOOOOOOOOO   OOOO     \n");
+        printf("      OOOOOO   OOOOOOOOO   OOOOOO      \n");
+        printf("         OOOOOO         OOOOOO         \n");
+        printf("             OOOOOOOOOOOO              \n");
+        printf("\n\n");
+
+        printf("Parabéns! Você ganhou!\n");
+        printf("Você acertou em %d tentativas!\n", tentativas);
+        printf("Total de pontos.: %.1f\n", pontos);
+
+} else {
+        printf("Você perdeu! Tente de novo!\n");
+
+        printf("       \\|/ ____ \\|/    \n");
+        printf("        @~/ ,. \\~@      \n");
+        printf("       /_( \\__/ )_\\    \n");
+        printf("          \\__U_/        \n");
+
+//...
+
+}
 
 
 }
